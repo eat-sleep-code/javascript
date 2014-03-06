526f7ebbf549db9365ab4fb2e5bcbefdee09857f
Eat Sleep Code <info@eat-sleep-code.com>
2014-03-06 11:57:11 -0700

    Create jquery.stylesheet.insertion.js

---
+++ jquery.stylesheet.insertion.js
@@ -0,0 +1,9 @@
+
+/* Placing a link to a stylesheet outside of the <head> tag is not valid, per the W3C spec. */
+/* This script will allow you to insert a new stylesheet into the <head> tag from anywhere on the page.
+/* Usage Example: insertStylesheet("/style/mypage.css");
+
+
+function insertStylesheet(stylesheetPath) {
+		$('head').append('<link rel="stylesheet" href="' + stylesheetPath + '" />'); 
+};
